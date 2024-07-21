22825ce160d1cae6f3f8a531dc9678eee5256b82
Luis Carlos Macêdo Moreira <luiscmacedo84@gmail.com>
2024-07-21 17:39:32 -0300

    initial commit

---
+++ src/test/java/com/dio/dio_project_bootcamp_java_2024/DioProjectBootcampJava2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.dio_project_bootcamp_java_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioProjectBootcampJava2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
